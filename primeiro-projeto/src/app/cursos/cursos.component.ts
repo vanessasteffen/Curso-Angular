1cc9cc77a1e11eed40e4543cec54f1210969e5cd
Vanessa Steffen <vanessasteffen186@gmail.com>
2022-05-11 16:53:45 -0300

    11/05/2022

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
+import {CursosService} from "./cursos.service";
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-cursos',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+
+  nomePortal:string;
+  cursos:string[];
+
+  constructor(cursoService: CursosService ) {
+    this.nomePortal = 'http://loiane.training';
+
+    /*for (let i=0; i=this.cursos.length; i++){
+      let curso = this.cursos[i];
+    }*/
+    var servico = new CursosService();
+  }
+
 
   ngOnInit(): void {
   }
